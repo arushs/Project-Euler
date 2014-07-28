350ed4b74149f790f74587ff2fcb4a36c72d0427
arushs <arushshankar@gmail.com>
2014-07-28 12:05:32 -0700

    fixed some wonky stuff in the repo

---
+++ solutions/problem3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number = 13195;
+}
+
+int nextPrime(int prev) {
+    if (prev == 1)
+        return 2;
+    else {
+        
+    }
+}
